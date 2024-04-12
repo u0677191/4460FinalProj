a6be0b6ec1daf404c8a9e0580d37ad17a8d1f4af
u0677191 <156018695+u0677191@users.noreply.github.com>
2024-04-11 19:00:29 -0600

    Michelle's Updates
    Added forms.py

--- FinalProject/athletic_department/views.py
+++ FinalProject/athletic_department/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-from .models import Team
+from django.shortcuts import render, redirect
+from .models import Team, Employee
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+
+
+from .forms import TeamForm, EmployeeForm
+def homepage(request):
+    return render(request, 'athletic_department/Homepage.html')
 
 def team_list(request):
     teams = Team.objects.all()
     return render(request, 'athletic_department/team_list.html', {'teams': teams})
 
-def homepage(request):
-    return render(request, 'athletic_department/Homepage.html')
+class TeamCreateView(CreateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'athletic_department/team_form.html'
+    success_url = '/teams/'
+
+class TeamUpdateView(UpdateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'athletic_department/team_form.html'
+    success_url = '/teams/'
+
+class TeamDeleteView(DeleteView):
+    model = Team
+    success_url = '/teams/'
