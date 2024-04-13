4aaff877220d4d8d7b37a2e86b8f4e87f506d291
u0677191 <156018695+u0677191@users.noreply.github.com>
2024-04-13 12:01:31 -0600

    04132024

--- FinalProject/athletic_department/views.py
+++ FinalProject/athletic_department/views.py
@@ -1,28 +1,9 @@
-from django.shortcuts import render, redirect
-from .models import Team, Employee
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView
-
-
-from .forms import TeamForm, EmployeeForm
-def homepage(request):
-    return render(request, 'athletic_department/Homepage.html')
+from django.shortcuts import render
+from .models import Team
 
 def team_list(request):
     teams = Team.objects.all()
     return render(request, 'athletic_department/team_list.html', {'teams': teams})
 
-class TeamCreateView(CreateView):
-    model = Team
-    form_class = TeamForm
-    template_name = 'athletic_department/team_form.html'
-    success_url = '/teams/'
-
-class TeamUpdateView(UpdateView):
-    model = Team
-    form_class = TeamForm
-    template_name = 'athletic_department/team_form.html'
-    success_url = '/teams/'
-
-class TeamDeleteView(DeleteView):
-    model = Team
-    success_url = '/teams/'
+def homepage(request):
+    return render(request, 'athletic_department/Homepage.html')
