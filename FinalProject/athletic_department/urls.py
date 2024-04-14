002154d98027b286a0ba671f692e6848dcb0a083
u0677191 <156018695+u0677191@users.noreply.github.com>
2024-04-13 19:14:02 -0600

    04132024-1914

--- FinalProject/athletic_department/urls.py
+++ FinalProject/athletic_department/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
-from .views import AthleteCreateView, TeamCreateView , AthleteUpdateView, AthleteDetailView, AthleteDeleteView, employee_login, employee_dashboard
+from .views import (AthleteCreateView, TeamCreateView , AthleteUpdateView, AthleteDetailView, AthleteDeleteView, 
+                    employee_login, employee_dashboard, athlete_delete, athlete_list, athlete_detail)
+
 
 
 urlpatterns = [
@@ -13,6 +15,15 @@ urlpatterns = [
     path('athlete/update/', AthleteUpdateView.as_view(), name='athlete_update'),
     path('athlete/detail/', AthleteDetailView.as_view(), name='athlete_detail'),
     path('athlete/<int:pk>/delete/', AthleteDeleteView.as_view(), name='athlete_delete'),
+
+    # Begin Felix's test URLS
+
+    path('athletes/', athlete_list, name='athlete_list'),
+    path('athlete/<str:athleteid>/', athlete_detail, name='athlete_detail'),
+    #path('athlete/<str:athleteid>/delete/', athlete_delete, name='athlete_delete'),
+    #path('athlete/create/', athlete_create, name='athlete_create'),
+
+    # End Felix's test URLS
 
     path('employee_login/', views.employee_login, name='employee_login'),
     path('employee_dashboard/', views.employee_dashboard, name='employee_dashboard'),
--- FinalProject/athletic_department/views.py
+++ FinalProject/athletic_department/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import Team, Employee , Athlete
@@ -6,6 +6,7 @@ from django.views.generic import CreateView, UpdateView, DeleteView, ListView , 
 from .forms import TeamForm, EmployeeForm, AthleteForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
 
 
 def homepage(request):
@@ -38,7 +39,7 @@ class TeamDeleteView(DeleteView):
 class AthleteListView(ListView):
     model = Athlete
     form_class = AthleteForm
-    template_name = 'athletic_department/athlete_form.html'
+    template_name = 'athletic_department/athlete_list.html'
     success_url = '/athletes/'
 
 class AthleteCreateView(CreateView):
@@ -59,26 +60,57 @@ class AthleteDetailView(DetailView):
     template_name = 'athletic_department/athlete_detail.html'
     success_url = '/athletes/'
 
+# class AthleteDeleteView(DeleteView):
+#    model = Athlete
+#    form_class = AthleteForm
+#    template_name = 'athletic_department/athlete_delete.html'
+#    success_url = '/athletes/'
+
 class AthleteDeleteView(DeleteView):
     model = Athlete
-    form_class = AthleteForm
-    template_name = 'athletic_department/athlete_delete.html'
-    success_url = '/athletes/'
+    template_name = 'athletic_department/athlete_confirm_delete.html'
+    success_url = reverse_lazy('athlete_list')
 
 #def employee_login(request):
    # return render(request, 'athletic_department/employee_login.html')
 
 def employee_login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
             login(request, user)
-            return redirect('employee_dashboard')  # Redirects to the employee dashboard
+            # Check if the user is a superuser
+            if user.is_superuser:
+                return HttpResponseRedirect(reverse('admin:index'))  # Redirect to Django admin
+            elif user.is_staff:
+                return redirect('employee_dashboard')  # Redirect to the staff dashboard
+            else:
+                # Optionally handle non-staff users differently
+                return redirect('some_general_page')
         else:
-            messages.error(request, "Invalid username or password.")
+            # Return an error message if login fails
+            return render(request, 'athletic_department/employee_login.html', {'error': 'Invalid username or password'})
+    # If not a POST request, show the login form
     return render(request, 'athletic_department/employee_login.html')
 
 def employee_dashboard(request):
     return render(request, 'athletic_department/employee_dashboard.html')
+
+def athlete_list(request):
+    athletes = Athlete.objects.all()  # Retrieves all athletes from the database
+    return render(request, 'athletic_department/athlete_list.html', {'athletes': athletes})
+
+def athlete_detail(request, athleteid):
+    # Retrieve an athlete by their 'athleteid'
+    athlete = get_object_or_404(Athlete, athleteid=athleteid)
+    return render(request, 'your_app_name/athlete_detail.html', {'athlete': athlete})
+
+def athlete_delete(request, pk):
+    athlete = get_object_or_404(Athlete, pk=pk)
+    if request.method == 'POST':
+        athlete.delete()
+        return redirect('athlete_list')
+    return render(request, 'athletic_department/athlete_confirm_delete.html', {'athlete': athlete})
